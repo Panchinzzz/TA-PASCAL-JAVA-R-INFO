1bb0fe730ac27cdb57c966757fcd97edb091f460
Valen <96664741+ValenUNLP@users.noreply.github.com>
2022-09-29 22:23:29 -0300

    Create MainEJ5.java

---
+++ JAVA/Clase3/EJ5/MainEJ5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema3;
+
+/**
+ *
+ * @author theVa
+ */
+public class MainEj5P3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Circulo MiCirculo = new Circulo(34, "Verde", "Amarillo");
+        System.out.println(MiCirculo.CalcularPerimetro()34);
+    }
+    
+}
